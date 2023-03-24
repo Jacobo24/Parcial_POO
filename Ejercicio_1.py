c5f8920b053b65055b83f4c388048cd67d0ff1c7
Jacobo24 <jaccalgar@gmail.com>
2023-03-24 10:53:51 +0100

    ejercicio 1

--- Ejercicio_1.py
+++ Ejercicio_1.py
@@ -3,3 +3,9 @@ class libro:
         self.titulo = input("Introduce el titulo del libro: ")
         self.autor = input("Introduce el autor del libro: ")
         self.paginas = int(input("Número de páginas del libro: "))
+        self.precio = int(input("Precio del libro: "))
+    def mostrar_libro(self):
+        print("Titulo: ", self.titulo)
+        print("Autor: ", self.autor)
+        print("Páginas: ", self.paginas)
+        print("Precio: ", self.precio)
