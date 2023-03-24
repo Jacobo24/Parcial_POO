48ce90d10f81db4a1f4ce3c2042abe4e57e28123
Jacobo24 <jaccalgar@gmail.com>
2023-03-24 12:10:10 +0100

    nada

--- Ejercicio_2/EJercicio_2.py
+++ Ejercicio_2/EJercicio_2.py
@@ -2,13 +2,12 @@ class Animal:
     def __init__(self, especie, tipo):
         self.especie = especie
         self.tipo = tipo
-    
     def mostrar_animal(self):
         print("Animal: ", self.especie, ", Tipo: ", self.tipo)
 
-animal1 = Animal("Pollo", "No se sabe", "Ovíparo")
+animal1 = Animal("Pollo", "No se dice", "Ovíparo")
 animal2 = Animal("Ornitorrinco", "Mamífero", "Ovíparo")
-animal3 = Animal("Gato", "Mamífero", "No se sabe")
+animal3 = Animal("Gato", "Mamífero", "No se dice")
 animal1.mostrar_animal()
 animal2.mostrar_animal()
 animal3.mostrar_animal()
