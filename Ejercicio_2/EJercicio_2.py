eeff2699a25fe4318a7f90f323138eab4f43d0bc
Jacobo24 <jaccalgar@gmail.com>
2023-03-24 11:11:56 +0100

    ejercicio 2 de python

---
+++ Ejercicio_2/EJercicio_2.py
@@ -0,0 +1,11 @@
+class animal:
+    class Mamífero:
+        class Ornitorrinco:
+            pass
+        class Gato:
+            pass
+    class Ovíparo:
+        class Ornitorrinco:
+            pass
+        class Pollo:
+            pass
