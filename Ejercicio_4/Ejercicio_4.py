5363295191408af63dc8538252dfafd6a2c38c29
Jacobo24 <jaccalgar@gmail.com>
2023-03-24 12:13:44 +0100

    ejercicio 4

---
+++ Ejercicio_4/Ejercicio_4.py
@@ -0,0 +1,7 @@
+class Cuenta_bancaria:
+    def __init__(self, nombre, saldo, numero, ID, Fecha ):
+        self.nombre = nombre
+        self.saldo = saldo
+        self.numero = numero
+        self.ID = ID
+        self.Fecha = Fecha
