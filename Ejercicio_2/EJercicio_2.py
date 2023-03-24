e65500fb9027d932235d68beedfce3bd46d9492b
Jacobo24 <jaccalgar@gmail.com>
2023-03-24 11:47:28 +0100

    cambios en el ejercicio 2

--- Ejercicio_2/EJercicio_2.py
+++ Ejercicio_2/EJercicio_2.py
@@ -1,11 +1,20 @@
 class animal:
-    class Mamífero:
-        class Ornitorrinco:
-            pass
-        class Gato:
-            pass
-    class Ovíparo:
-        class Ornitorrinco:
-            pass
-        class Pollo:
-            pass
+    def __init__(self, Pollo, Ornitorrinco, Gato):
+        self.animal1 = input(Pollo)
+        self.animal2 = input(Ornitorrinco)
+        self.animal3 = input(Gato)
+        self.tipo = input("Su tipo es ovíparo")
+        self.especie = input("Su especie es mamífero")
+        if Pollo:
+            print("Es un animal ovíparo")
+        elif Ornitorrinco:
+            print("Es un animal mamífero y ovíparo")
+        elif Gato:
+            print("Es un animal mamífero")
+    def mostrar_animal(self):
+        print("Animal: ", self.animal1)
+        print("Animal: ", self.animal2)
+        print("Animal: ", self.animal3)
+
+def main():
+    print("Escoge un animal entre pollo, ornitorrinco y gato")
