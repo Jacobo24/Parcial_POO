a1a774cb2e90a1898104e9a02d1bb3f5234c0967
Jacobo24 <jaccalgar@gmail.com>
2023-03-24 12:04:53 +0100

    fin del ejercicio 2

--- Ejercicio_2/EJercicio_2.py
+++ Ejercicio_2/EJercicio_2.py
@@ -1,20 +1,14 @@
-class animal:
-    def __init__(self, Pollo, Ornitorrinco, Gato):
-        self.animal1 = input(Pollo)
-        self.animal2 = input(Ornitorrinco)
-        self.animal3 = input(Gato)
-        self.tipo = input("Su tipo es ovíparo")
-        self.especie = input("Su especie es mamífero")
-        if Pollo:
-            print("Es un animal ovíparo")
-        elif Ornitorrinco:
-            print("Es un animal mamífero y ovíparo")
-        elif Gato:
-            print("Es un animal mamífero")
+class Animal:
+    def __init__(self, especie, tipo):
+        self.especie = especie
+        self.tipo = tipo
+    
     def mostrar_animal(self):
-        print("Animal: ", self.animal1)
-        print("Animal: ", self.animal2)
-        print("Animal: ", self.animal3)
+        print("Animal: ", self.especie, ", Tipo: ", self.tipo)
 
-def main():
-    print("Escoge un animal entre pollo, ornitorrinco y gato")
+animal1 = Animal("Pollo", "No se sabe", "Ovíparo")
+animal2 = Animal("Ornitorrinco", "Mamífero", "Ovíparo")
+animal3 = Animal("Gato", "Mamífero", "No se sabe")
+animal1.mostrar_animal()
+animal2.mostrar_animal()
+animal3.mostrar_animal()
