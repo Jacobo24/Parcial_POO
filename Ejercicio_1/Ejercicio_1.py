17f4c1dcfb75692cbea6fcedebf86e914cc85fed
Jacobo24 <jaccalgar@gmail.com>
2023-03-24 11:11:11 +0100

    corrección ejercicio 1

--- Ejercicio_1/Ejercicio_1.py
+++ Ejercicio_1/Ejercicio_1.py
@@ -1,5 +1,5 @@
 class libro:
-    def describir_libro(self):
+    def __init__(self):
         self.titulo = input("Introduce el titulo del libro: ")
         self.autor = input("Introduce el autor del libro: ")
         self.paginas = int(input("Número de páginas del libro: "))
